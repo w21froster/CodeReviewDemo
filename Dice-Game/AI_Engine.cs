ad3821b00ed66321fbf183afaed4387c67b94d1f
w21froster <fosta.w@gmail.com>
2022-11-14 12:02:15 -0800

    Dice game prod (#4)
    * adding dice game
    
    * adding gitignore
    
    * remove ignored files

---
+++ Dice-Game/AI_Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+    internal class AI_Engine
+    {
+        private int highestDice { get; set; }
+        private Random rnd;
+
+        public AI_Engine()
+        {
+            highestDice = 19;
+            rnd = new Random();
+        }
+
+        public int AI_Roll()
+        {
+            int returnVar = rnd.Next(highestDice+1);
+            return returnVar;
+        }   
+    }
+}
